3f4af1148e403a1bc2208ce0fe416a59a31369d4
seulahhh <ssseulahhh@gmail.com>
2024-10-23 14:54:16 +0900

    Create Algoritm_04.java
    알고리즘 - 이진탐색

---
+++ Algoritm_04.java
@@ -0,0 +1,38 @@
+// 알고리즘 - 이진 탐색
+
+public class Main {
+    // 반복문 구조
+    public static int binarySearch(int arr[], int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (target == arr[mid]) {
+                return mid;
+            } else if (target < arr[mid]) {
+                right = mid -1; // 중앙값을 제외 한 왼쪽 덩어리;
+            } else if (target > arr[mid]) {
+                left = mid + 1;
+            }
+        }
+
+        return -1; // 찾으려는 데이터가 없다는 뜻으로 -1을 출력해준다.
+    }
+    
+    // 재귀 호출 구조
+    public static int binarySearch2(int[] arr, int target, int left, int right) {
+        
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 5, 10, 20, 30, 40, 50, 60};
+
+        System.out.println("Index: " + binarySearch(arr, 30));
+        System.out.println();
+
+        System.out.println("Index: " + bi narySearch2(arr, 30, 0, arr.length - 1));
+    }
+}
